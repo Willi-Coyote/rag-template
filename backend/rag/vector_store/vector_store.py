2ab7193dcb76871790e7be0ca6b8e08f4559ab93
Ria Nagpal <ria.nagpal@obviohealth.com>
2024-04-30 13:11:44 +0800

    delete everything in backend

--- backend/rag/vector_store/vector_store.py
+++
@@ -1,8 +0,0 @@
-from typing import Protocol, List, Any
-
-from langchain.schema import Document
-
-
-class VectorStore(Protocol):
-    def add_documents(self, documents: List[Document], **kwargs: Any) -> List[str]:
-        ...
